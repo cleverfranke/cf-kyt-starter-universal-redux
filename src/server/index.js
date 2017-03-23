5d2f0e316e1d94a6805f77278d68de8152c26189
Jan Hoogeveen <jan@cleverfranke.com>
2017-03-23 17:35:47 +0100

    Force a build in Travis after a rebase

--- src/server/index.js
+++ src/server/index.js
@@ -80,6 +80,7 @@ app.use((request, response) => {
 
         // Needed to prevent memory leaks on the server
         // https://github.com/nfl/react-helmet#server-usage
+        // 
         const helmet = Helmet.renderStatic();
       };
 
