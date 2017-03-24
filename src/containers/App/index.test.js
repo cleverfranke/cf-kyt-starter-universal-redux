711f54c2c0d868359f30d8d166283f3a88cea890
Jan Hoogeveen <jan@cleverfranke.com>
2017-03-24 17:46:04 +0100

    Fixes the App does not have a children prop error

--- src/containers/App/index.test.js
+++ src/containers/App/index.test.js
@@ -5,6 +5,6 @@ import { shallow } from 'enzyme';
 import App from './';
 
 it('should render the App container as a div element', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = shallow(<App>Test</App>);
   expect(wrapper.is('div')).toBeTruthy();
 });
