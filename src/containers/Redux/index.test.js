5bc20ff63e666ac37c572da95bfc5a6ccb718af5
Jan Hoogeveen <jan@cleverfranke.com>
2017-04-07 15:36:52 +0200

    Adds some extra unit tests to the Redux Container

--- src/containers/Redux/index.test.js
+++ src/containers/Redux/index.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ReduxContainer } from './';
 
+import {
+  ReduxContainer,
+  mapStateToProps,
+  mapDispatchToProps,
+} from './';
+
+import { shouldIncrement } from '../../redux/reducers/modules/counter';
 const handleIncrement = () => {};
 
-it('should render the Redux container as a section element', () => {
-  const wrapper = shallow(<ReduxContainer shouldIncrement={handleIncrement} />);
-  expect(wrapper.is('section')).toBeTruthy();
+describe('The Redux container', () => {
+  it('should render as a section element', () => {
+    const wrapper = shallow(<ReduxContainer shouldIncrement={handleIncrement} />);
+    expect(wrapper.is('section')).toBeTruthy();
+  });
+
+  it('should return an empty object in the mapStateToProps', () => {
+    const state = mapStateToProps();
+    const expectedState = {};
+    expect(state).toEqual(expectedState);
+  });
+
+  it('should return an object of actions when calling mapDispatchToProps', () => {
+    const state = mapDispatchToProps();
+    const expectedState = {
+      shouldIncrement,
+    };
+    expect(state).toEqual(expectedState);
+  });
 });
